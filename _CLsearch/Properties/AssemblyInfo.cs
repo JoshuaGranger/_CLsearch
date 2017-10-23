8ea33db01c1ec28af7c3d164fd47819532e85108
Joshua Granger <joshuagranger13@gmail.com>
2017-10-22 19:36:20 -0500

    Minor Changes

--- _CLsearch/Properties/AssemblyInfo.cs
+++ _CLsearch/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("_CLsearch")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Joshua Granger")]
 [assembly: AssemblyProduct("_CLsearch")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
